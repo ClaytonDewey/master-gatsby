da31d44e838ccc13a5396f6a37723153dc0047e0
ClaytonDewey <clay@dryadmedia.com>
2026-04-13 15:15:56 -0500

    Finish topping filter & add beers list

--- gatsby/src/components/ToppingsFilter.js
+++ gatsby/src/components/ToppingsFilter.js
@@ -19,7 +19,7 @@ const ToppingsStyles = styled.div`
       background: white;
       padding: 2px 5px;
     }
-    .active {
+    &[aria-current='page'] {
       background: var(--yellow);
     }
   }
@@ -53,18 +53,11 @@ function countPizzasInToppings(pizzas) {
   return sortedToppings;
 }
 
-export default function ToppingsFilter() {
+export default function ToppingsFilter({ activeTopping }) {
   // Get a list of all the toppings
   // Get a list of all the Pizzas with their toppings
-  const { toppings, pizzas } = useStaticQuery(graphql`
+  const { pizzas } = useStaticQuery(graphql`
     query {
-      toppings: allSanityTopping {
-        nodes {
-          name
-          id
-          vegetarian
-        }
-      }
       pizzas: allSanityPizza {
         nodes {
           toppings {
@@ -82,6 +75,10 @@ export default function ToppingsFilter() {
   // Link it up.. ... . . .
   return (
     <ToppingsStyles>
+      <Link to='/pizzas'>
+        <span className='name'>All</span>
+        <span className='count'>{pizzas.nodes.length}</span>
+      </Link>
       {/* Loop over the list of toppings and display the topping and the count of pizzas in that topping */}
       {toppingsWithCounts.map((topping) => (
         <Link to={`/topping/${topping.name}`} key={topping.id}>
--- gatsby/src/pages/beers.js
+++ gatsby/src/pages/beers.js
@@ -1,9 +1,44 @@
 import React from 'react';
+import { graphql } from 'gatsby';
 
-export default function BeersPage() {
+export default function BeersPage({ data }) {
   return (
     <>
-      <p>Hey! I'm the beers page.</p>
+      <h2 className='center'>
+        We have {data.beers.nodes.length} Beers Available. Dine in Only!
+      </h2>
+      <div>
+        {data.beers.nodes.map((beer) => {
+          // const rating = Math.round(beer.rating.average);
+          return (
+            <div key={beer.id}>
+              <img src={beer.image} alt={beer.name} />
+              <h3>{beer.name}</h3>
+              {beer.price}
+              {/* <p title={`${rating} out of 5 stars`}>
+                {`⭐️`.repeat(rating)}
+                <span style={{ filter: `grayscale(100%)`}}>
+                  {`⭐️`.repeat(5 - rating)}
+                </span>
+                <span>({beer.rating.reviews})</span>
+              </p> */}
+            </div>
+          );
+        })}
+      </div>
     </>
   );
 }
+
+export const query = graphql`
+  query {
+    beers: allBeer {
+      nodes {
+        id
+        name
+        price
+        image
+      }
+    }
+  }
+`;
