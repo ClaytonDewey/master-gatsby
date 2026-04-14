f47454cd0a63726cb67c8d5d016d08b137a98b5c
ClaytonDewey <clay@dryadmedia.com>
2026-04-14 07:48:13 -0500

    Change beers to stouts for ratings

--- gatsby/gatsby-node.js
+++ gatsby/gatsby-node.js
@@ -65,7 +65,7 @@ async function fetchBeersAndTurnIntoNodes({
   createContentDigest,
 }) {
   // 1. Fetch a  list of beers
-  const res = await fetch('https://api.sampleapis.com/beers/ale');
+  const res = await fetch('https://api.sampleapis.com/beers/stouts');
   const beers = await res.json();
   // 2. Loop over each one
   for (const beer of beers) {
